f7e18c1e16866ae2d5ce64dd9d17d914407cbf12
Fabian <fabiananguiano@gmail.com>
2024-05-30 15:21:50 -0500

    Update deploy_flask_app.sh

--- deploy_flask_app.sh
+++ deploy_flask_app.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get the directory argument
+APP_DIR=$1
+
 # Update and install necessary packages
 sudo apt update && sudo apt install -y python3 python3-venv nginx apt python3-pip
 
@@ -18,13 +21,11 @@ cat << EOF | sudo tee /etc/systemd/system/myflaskapp.service
 [Unit]
 Description=Gunicorn instance to serve Flask app
 After=network.target
-
 [Service]
 User=root
 Group=www-data
-WorkingDirectory=/root/flask_deploy_script
-ExecStart=/root/flask_deploy_script/venv/bin/gunicorn --workers 3 --bind unix:/root/flask_deploy_script/myflaskapp.sock -m 000 app:app
-
+WorkingDirectory=/root/flask_deploy_script/$APP_DIR
+ExecStart=/root/flask_deploy_script/venv/bin/gunicorn --workers 3 --bind unix:/root/flask_deploy_script/$APP_DIR/myflaskapp.sock -m 000 app:app
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -35,7 +36,7 @@ sudo systemctl restart myflaskapp
 sudo systemctl enable myflaskapp
 
 # Change permissions of the socket to be universally accessible, change this if you move to production. 
-sudo chmod 777 /root/flask_deploy_script/myflaskackapp.sock
+sudo chmod 777 /root/flask_deploy_script/$APP_DIR/myflaskapp.sock
 sudo chmod a+x /root
 sudo chmod a+x /root/flask_deploy_script
 
@@ -44,15 +45,14 @@ cat << EOF | sudo tee /etc/nginx/sites-available/myflaskapp
 server {
     listen 80;
     server_name _;
-
     location / {
         include proxy_params;
-        proxy_pass http://unix:/root/flask_deploy_script/myflaskapp.sock;
+        proxy_pass http://unix:/root/flask_deploy_script/$APP_DIR/myflaskapp.sock;
     }
 }
 EOF
 
-# Enable the site and restart Nginjab
+# Enable the site and restart Nginx
 sudo ln -sf /etc/nginx/sites-available/myflaskapp /etc/nginx/sites-enabled/
 sudo nginx -t
 sudo systemctl restart nginx
