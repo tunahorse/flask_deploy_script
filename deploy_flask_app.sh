58eb05b55cf4fb1ba577fcd511a5efb5ae724010
Fabian <fabiananguiano@gmail.com>
2024-05-30 15:02:37 -0500

    Update deploy_flask_app.sh

--- deploy_flask_app.sh
+++ deploy_flask_app.sh
@@ -4,8 +4,8 @@
 sudo apt update && sudo apt install -y python3 python3-venv nginx apt python3-pip
 
 # Setup the virtual environment and activate it
-python3 -m venv /root/helloworld/venv
-source /root/helloworld/venv/bin/activate
+python3 -m venv /root/flask_deploy_script/venv
+source /root/flask_deploy_script/venv/bin/activate
 
 # Install Flask and Gunicorn within the virtual environment
 pip install Flask gunicorn
@@ -22,8 +22,8 @@ After=network.target
 [Service]
 User=root
 Group=www-data
-WorkingDirectory=/root/helloworld
-ExecStart=/root/helloworld/venv/bin/gunicorn --workers 3 --bind unix:/root/helloworld/myflaskapp.sock -m 000 app:app
+WorkingDirectory=/root/flask_deploy_script
+ExecStart=/root/flask_deploy_script/venv/bin/gunicorn --workers 3 --bind unix:/root/flask_deploy_script/myflaskapp.sock -m 000 app:app
 
 [Install]
 WantedBy=multi-user.target
@@ -35,9 +35,9 @@ sudo systemctl restart myflaskapp
 sudo systemctl enable myflaskapp
 
 # Change permissions of the socket to be universally accessible, change this if you move to production. 
-sudo chmod 777 /root/helloworld/myflaskackapp.sock
+sudo chmod 777 /root/flask_deploy_script/myflaskackapp.sock
 sudo chmod a+x /root
-sudo chmod a+x /root/helloworld
+sudo chmod a+x /root/flask_deploy_script
 
 # Setup Nginx configuration
 cat << EOF | sudo tee /etc/nginx/sites-available/myflaskapp
@@ -47,7 +47,7 @@ server {
 
     location / {
         include proxy_params;
-        proxy_pass http://unix:/root/helloworld/myflaskapp.sock;
+        proxy_pass http://unix:/root/flask_deploy_script/myflaskapp.sock;
     }
 }
 EOF
